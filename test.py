55c9ff85be804d8ac5c0b93cbe8b1bd3ee0f7f85
RHA1705 <garbarzroman47@gmail.com>
2024-11-28 22:28:37 +0100

    final2

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import json
 from bs4 import BeautifulSoup
 
 
+
+def delete_duplicates(data):
+    new_data = set(data)
+    return new_data
 def scrape_authors(page_url):
     response = requests.get(page_url)
     if response.status_code != 200:
@@ -18,27 +22,27 @@ def scrape_authors(page_url):
         author_link = quote.find('small', class_='author').find_next_sibling('a')['href']
         author_links.append(author_link)
 
-    author_links_not_repeat = set(author_links)
+    # author_links_not_repeat = delete_duplicates(author_links)
     base_url = "http://quotes.toscrape.com"
-    for author_l in author_links_not_repeat:
-        new_url = f'{base_url}{author_l}'  # Збираємо  повний URL автора
-        new_response = requests.get(new_url)
-        if new_response.status_code != 200:
-            print(f"Failed to retrieve author page: {new_url}")
-            continue
-        new_soup = BeautifulSoup(new_response.text, 'lxml')
-        author_details = new_soup.find('div', class_='author-details')
-        if author_details:
-            name = author_details.find('h3', class_='author-title').get_text(strip=True)
-            born_date = author_details.find('span', class_='author-born-date').get_text(strip=True)
-            born_location = author_details.find('span', class_='author-born-location').get_text(strip=True)
-            description = author_details.find('div', class_='author-description').get_text(strip=True)
-            authors_data.append({
-                'fullname': name,
-                'born_date': born_date,
-                'born_location': born_location,
-                'description': description
-            })
+    # for author_l in author_links_not_repeat:
+    #     new_url = f'{base_url}{author_l}'  # Збираємо  повний URL автора
+    #     new_response = requests.get(new_url)
+    #     if new_response.status_code != 200:
+    #         print(f"Failed to retrieve author page: {new_url}")
+    #         continue
+    #     new_soup = BeautifulSoup(new_response.text, 'lxml')
+    #     author_details = new_soup.find('div', class_='author-details')
+    #     if author_details:
+    #         name = author_details.find('h3', class_='author-title').get_text(strip=True)
+    #         born_date = author_details.find('span', class_='author-born-date').get_text(strip=True)
+    #         born_location = author_details.find('span', class_='author-born-location').get_text(strip=True)
+    #         description = author_details.find('div', class_='author-description').get_text(strip=True)
+    #         authors_data.append({
+    #             'fullname': name,
+    #             'born_date': born_date,
+    #             'born_location': born_location,
+    #             'description': description
+    #         })
 
     # Перевірка чи є кнопка
     next_button = soup.find('li', class_='next')
@@ -46,7 +50,7 @@ def scrape_authors(page_url):
     if next_button:
         next_relative_url = next_button.find('a')['href']
         next_page_url = f"{base_url}{next_relative_url}"  # Збираємо урлу для перехода
-    return authors_data, next_page_url
+    return author_links, next_page_url
 
 
 def main():
@@ -57,12 +61,14 @@ def main():
     while current_url:
         print(f"Scraping {current_url}...")
         authors, next_page_url = scrape_authors(current_url)
+
         all_authors.extend(authors)
+
         current_url = next_page_url  # Переходимо на наступну сторінку
 
     print(f"Total authors collected: {len(all_authors)}")
 
-    with open('authors.json', 'w', encoding='utf-8') as f:
+    with open('authors1.json', 'w', encoding='utf-8') as f:
         json.dump(all_authors, f, indent=4, ensure_ascii=False)
 
 
